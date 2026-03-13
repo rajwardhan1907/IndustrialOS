28233662984392f27aa4dea805bb04cca593dcc4
Rajwardhan Pawar <rajwardhan0707@gmail.com>
2026-03-13 19:34:23 +0530

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token, req }) => {
+        const { pathname } = req.nextUrl;
+        if (pathname.startsWith("/portal"))   return true;
+        if (pathname.startsWith("/login"))    return true;
+        if (pathname.startsWith("/api/auth")) return true;
+        return !!token;
+      },
+    },
+  }
+);
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
