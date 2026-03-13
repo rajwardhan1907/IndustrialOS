a19e123b9f5182feb5e8ba73397d82e2c4c31a12
Rajwardhan Pawar <rajwardhan0707@gmail.com>
2026-03-14 00:49:20 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,11 @@
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
-  pages: {
-    signIn: "/login",
-  },
+  pages: { signIn: "/login" },
 });
 
 export const config = {
-  // Protect everything EXCEPT these public routes
   matcher: [
-    "/((?!login|portal|api/auth|_next/static|_next/image|favicon.ico|icon-|manifest.json|robots.txt).*)",
+    "/((?!login|portal|api/auth|_next/static|_next/image|favicon.ico).*)",
   ],
 };
